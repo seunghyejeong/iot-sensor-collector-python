af6649093407c88caec37d0fc9c2f8d4c586027d
jeongbami <110725725+seunghyejeong@users.noreply.github.com>
2024-10-02 17:35:02 +0900

    main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ port = 1883
 topic = "test/topic"
 client_id = "python_mqtt_client"
 
-def connect_mqtt() -> mqtt.Client :
+def connect_mqtt() -> mqtt.Client:
 
     client = mqtt.Client(client_id=client_id, clean_session=True, userdata=None, callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
     client.subscribe(topic=topic)
@@ -37,30 +37,36 @@ def connect_mqtt() -> mqtt.Client :
     def on_log(client, userdata, level, buf):
         print(f"log: {buf}")
     
+    # 메시지 수신 콜백 함수
+    def on_message(client, userdata, message):
+        print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
-    client.on_log=on_log
+    client.on_log = on_log
+    client.on_message = on_message  # 메시지 수신 콜백 설정
+
     client.connect(host=host, port=port)
-    client.loop_start
+    client.loop_start()  # 비동기 MQTT 루프 시작
     return client
 
 @app.route('/acceleration', methods=['POST'])
 def postAcceleration():
     data = request.json
 
-    print("Received data:" ,data)
+    print("Received data:", data)
 
-    if data is None :
+    if data is None:
         return jsonify("No data"), 400
     try:
-        acceleration_data = AccelerationData(data['x'],data['y'],data['z'])
+        acceleration_data = AccelerationData(data['x'], data['y'], data['z'])
     except KeyError:
         return jsonify({"error": "Acceleration Data missing in JSON data"}), 400
 
     return jsonify({"message": "Acceleration Data received",
                     "x": acceleration_data.x,
                     "y": acceleration_data.y,
-                    "Z": acceleration_data.z}), 200
+                    "z": acceleration_data.z}), 200
 
 @app.route('/location', methods=['POST'])
 def postLocation():
@@ -71,11 +77,11 @@ def postLocation():
     try:
         location_data = LocationData(data['latitude'], data['longitude'])
     except KeyError:
-        return jsonify({"error" : "Location data missing in JSON data"}), 400
+        return jsonify({"error": "Location data missing in JSON data"}), 400
 
-    return jsonify({"message" : "Location Data received",
-                    "latitude" : location_data.latitude,
-                    "longitude" : location_data.longitude}), 200
+    return jsonify({"message": "Location Data received",
+                    "latitude": location_data.latitude,
+                    "longitude": location_data.longitude}), 200
 
 if __name__ == '__main__':
     connect_mqtt()
